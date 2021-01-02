dea1596985f1f08efeedf054bbe87a88b056929c
tkddlf4209 <tkddlf4209@naver.com>
2021-01-02 21:22:43 +0900

    B

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
-var request = require('request');
 var axios = require('axios').default;
-request.defaults({
-    headers: {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
-	    'Cache-Control': 'private,no-cache, no-store, must-revalidate,max-age=0',
-        'Pragma': 'no-cache',
-        'Expires': 0
-    }
-})
+
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json({type: 'application/json'}));
 app.get('/', function (req, res) {
   res.send('Bitpump Cralwer Server :)');
 });
- 
+
 app.post('/proxy', function (req, res) {
   console.log('url',  req.body.url); 
   if (!!req.body.url) {
