31526ac3604b1e3412aab6240da9c975dd31a1bc
tkddlf4209 <tkddlf4209@gmail.com>
2021-03-11 12:36:05 +0900

    B

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function getTimeMilis() {
 var flag = true;
 var time_stamp = getTimeMilis();
 var err = false;
-
+upbitRequest();
 startUpbitProjectCrawler(1800,true) // 서버가 시작되면 5초마다 실행하는 크롤러를 일단 실행하고 crawler manager에서 크롤링 요청을 하면 그떄는 빠른 크롤링을 실행
 setInterval(function(){
   if(err){
