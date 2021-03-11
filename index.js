e46ff4709cb3afc035ba9332228ad0b6f7dfee9b
tkddlf4209 <tkddlf4209@gmail.com>
2021-03-11 22:54:49 +0900

    B

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ var err = false;
 //startUpbitProjectCrawler(1800,true) // 서버가 시작되면 5초마다 실행하는 크롤러를 일단 실행하고 crawler manager에서 크롤링 요청을 하면 그떄는 빠른 크롤링을 실행
 setInterval(function(){
   if(err){
-    //selfRestart();
+    selfRestart();
   }
 },1050)
 
